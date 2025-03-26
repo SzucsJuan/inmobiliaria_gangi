8dfe144d2bf2b77cac12c006ebc814930d165f5e
IanBarreto332 <ianbarreto99@gmail.com>
2025-03-25 21:01:37 -0300

    Add backend functions

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const API_KEY = process.env.API_KEY;
+
+console.log('The API key is: ${API_KEY}');
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/data', (req, res) => {
+    res.json({  message: 'API ready' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
