2ffe76d0d1ac76750bd12df1f79665b6470d51e1
Juan Szucs <u63194@msm.gov.ar>
2025-04-07 10:45:42 -0300

    Hamburguer button fixed. Responsive for home page added

--- front/src/js/hamburger-nav.js
+++ front/src/js/hamburger-nav.js
@@ -1,7 +1,11 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navLinks = document.querySelector('.nav-links');
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const navLinks = document.querySelector('.nav-links');
 
-hamburgerMenu.addEventListener('click', () => {
-  navLinks.classList.toggle('show'); 
-  hamburgerMenu.classList.toggle('active');
+  if (hamburgerMenu && navLinks) {
+    hamburgerMenu.addEventListener('click', () => {
+      navLinks.classList.toggle('show'); 
+      hamburgerMenu.classList.toggle('active');
+    });
+  }
 });
